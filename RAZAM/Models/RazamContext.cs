227c163eb8f46993a4629eef9aa3c709fb391480
ProkopenkovD <62955842+ProkopenkovD@users.noreply.github.com>
2020-10-07 11:52:39 +0300

    Created project, added Model (Event) and cleared views

--- RAZAM/Models/RazamContext.cs
+++ RAZAM/Models/RazamContext.cs
@@ -2,10 +2,12 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.Data.Entity;
 
 namespace RAZAM.Models
 {
-    public class RazamContext
+    public class RazamContext : DbContext
     {
+        public DbSet<Event> Events { get; set; }
     }
 }
