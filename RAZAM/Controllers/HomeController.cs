81f7278552ba021be20e9250fd2addca59a54b4c
ProkopenkovDaniel <72033214+ProkopenkovDaniel@users.noreply.github.com>
2020-10-12 18:11:18 +0300

    Added redirect

--- RAZAM/Controllers/HomeController.cs
+++ RAZAM/Controllers/HomeController.cs
@@ -31,8 +31,9 @@ namespace RAZAM.Controllers
         {
             db.Events.Add(ev);
             db.SaveChanges();
-            var events = db.Events;
-            return View("Events", events.ToList());
+            //var events = db.Events;
+            //return View("Events", events.ToList());
+            return Redirect("/Home/Events");
         }
 
         [HttpGet]
@@ -56,6 +57,25 @@ namespace RAZAM.Controllers
             ViewBag.Users = db.Users.ToList();
             var files = db.Files;
             return View(files.ToList());
+        }
+
+        [HttpPost]
+        public ActionResult AddFile(File fi)
+        {
+            //Код поддерживающий введение польщователя в БД (должен быть изменён)
+            var files = db.Files;
+            File fiFind = db.Files.Find(fi.User);
+            if (fiFind != null)
+            {
+                fi.UserId = fiFind.Id;
+                db.Files.Add(fi);
+                db.SaveChanges();
+                files = db.Files;
+                return View("Files", files.ToList());
+            }
+            //files = db.Files;
+            //return View("Files", files.ToList());
+            return Redirect("/Home/Files");
         }
     }
 }
