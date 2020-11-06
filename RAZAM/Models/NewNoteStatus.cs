2f7c74687dceb231d45a6142a01ba1f3a5fa801d
ProkopenkovD <62955842+ProkopenkovD@users.noreply.github.com>
2020-11-06 16:25:45 +0300

    Add New Model
    NewNoteStatus - model

---
+++ RAZAM/Models/NewNoteStatus.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace RAZAM.Models
+{
+    public class NewNoteStatus
+    {
+        public int id { get; set; }
+        public State NewStatus { get; set; }
+    }
+}
